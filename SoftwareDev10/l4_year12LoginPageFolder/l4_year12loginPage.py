29c6920f5cbc1a2a416ca30c4296df22a4fd38a7
AntiAntiexe <mrbricken@gmail.com>
2025-02-01 17:54:38 +1100

    The app now checks for the username and password correctly and will the switch page

--- SoftwareDev10/l4_year12LoginPageFolder/l4_year12loginPage.py
+++ SoftwareDev10/l4_year12LoginPageFolder/l4_year12loginPage.py
@@ -35,7 +35,10 @@ class logInPage:
 
         # Title
         self.label = CTkLabel(master=app, text="Login Screen!", font=self.font_title)
-        self.label.place(relx=0.5, rely=0.1, anchor=CENTER)
+        
+
+        self.check = CTkLabel(master=app, text="Incorrect username or password!", font=(self.font_para, 20))
+        
 
         self.username = CTkEntry(master=app, placeholder_text="Username", width=185, height=40,fg_color='#0fa4af',
                               border_color='#0d737a', text_color="black", placeholder_text_color='#323231',
@@ -48,7 +51,12 @@ class logInPage:
         
         self.button = CTkButton(master=app, text="Submit", command=self.submit, fg_color="#0fa4af",
                                    border_color='#0d737a',
-                                   border_width=2, hover_color='#024950', text_color="#323231", height=40, width=185,
+                                   border_width=2, hover_color='#024950', text_color="#323231", height=20, width=100,
+                                   font=(self.font_para, 20))
+        
+        self.newAccBut = CTkButton(master=app, text="Create new account", command=self.createNewAcc, fg_color="#0fa4af",
+                                   border_color='#0d737a',
+                                   border_width=2, hover_color='#024950', text_color="#323231", height=10, width=50,
                                    font=(self.font_para, 20))
           
     def show(self):
@@ -57,19 +65,43 @@ class logInPage:
         self.username.place(relx=0.5, rely=0.2, anchor=CENTER)
         self.password.place(relx=0.5, rely=0.3, anchor=CENTER)
         self.button.place(relx=0.5, rely=0.4, anchor=CENTER)
+        self.newAccBut.place(relx=0.8, rely=0.95, anchor=CENTER)
+        
 
     def hide(self):
         self.label.place_forget()
         self.username.place_forget()
         self.password.place_forget()
         self.button.place_forget()
+        self.check.place_forget()
+        self.newAccBut.place_forget()
+
+    def createNewAcc(self):
+        pass
 
     def submit(self):
-        data = pd.read_csv("accounts.csv")
+        df = pd.read_csv('SoftwareDev10/l4_year12LoginPageFolder/accounts.csv')
+        user = self.username.get()
+        password = self.password.get()
+
+        data = df[df['username']== user].astype(str)
+        print(password, type(password))
+        print(data['password'].tolist())
+
+        print('\n ------------\n')
+        print(df['username'].tolist())
+
+        if user in df['username'].tolist() and password in data['password'].tolist():
+            print(f'hello {user}.')
+            show_page2()
+        else:
+            self.check.place(relx=0.5, rely=0.5, anchor=CENTER)
+            print('Incorrect username or password.')
+
+
+        print(df[df['username']== user])
 
         
-
-        show_page2()
         
 class mainPage:
     def __init__(self):
