85d5f92f8ba973cdf601fcb5d0a16b4acaf7a22c
AntiAntiexe <mrbricken@gmail.com>
2025-02-01 13:45:12 +1100

    create a username and password checker

--- SoftwareDev10/L3_forloops.py
+++ SoftwareDev10/L3_forloops.py
@@ -1,3 +1,20 @@
-strings = 0
+import pandas as pd
 
-print(strings)
+df = pd.read_csv('SoftwareDev10/l4_year12LoginPageFolder/accounts.csv')
+user = input('User: ')
+password = int(input('Pass: '))
+
+#trial_username = 'bob12'
+
+data = df[df['username']== user]
+
+if user in df['username'].tolist() and password in data['password'].to_list():
+    print(f'hello {user}.')
+else:
+    print('Incorrect username or password.')
+
+
+#selected_rows = df[df['Name'] == 'Alice']
+#print(selected_rows)
+
+print(df[df['username']== user])
