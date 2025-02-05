b936de0c71d30ff68e2350b2956de9a2b485e7ac
AntiAntiexe <mrbricken@gmail.com>
2025-02-05 14:23:57 +1100

    Create an app to calculate the total amount spent at a shop

---
+++ ipo_Shoppingcart.py
@@ -0,0 +1,25 @@
+price = int(input('Price of item: '))
+quantity = int(input('Quantitiy of item: '))
+x = True
+prices = []
+quantities = []
+while x:
+    prices.append(price)
+    quantities.append(quantity)
+    try:
+        price = int(input('Price of item: '))
+        quantity = int(input('Quantitiy of item: '))
+    except ValueError:
+        x = False
+    
+total = []
+for i in range(len(prices)):
+    total.append(prices[i]*quantities[i])
+
+withTax = []
+for i in total:
+    withTax.append(i * 1.1)
+
+
+print(f'Your total {sum(withTax)}')
+
