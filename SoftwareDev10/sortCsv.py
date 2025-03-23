d78e7e853b4367dfc65ba0791a2fcfce864d3af1
AntiAntiexe <mrbricken@gmail.com>
2025-03-24 10:57:10 +1100

    just gotta creatye a string file

--- SoftwareDev10/sortCsv.py
+++ SoftwareDev10/sortCsv.py
@@ -31,13 +31,13 @@ class App:
         self.label = CTkLabel(master=self.app, text="CSV Sorter", font=self.my_font)
         self.label.grid(column=0, row=0, padx=20, pady=20)
 
-        self.btn_select_num = CTkButton(master=self.app, text="Select Number File", command=self.select_file, fg_color="#0fa4af",
+        self.btn_select_num = CTkButton(master=self.app, text="Select Number File", command=self.select_Numfile, fg_color="#0fa4af",
                                    border_color='#0d737a',
                                    border_width=2, hover_color='#024950', text_color="#323231", height=40,
                                    font=(self.my_font2, 25))
         self.btn_select_num.grid(column=0, row=1, padx=20, pady=20)
 
-        self.btn_select_string = CTkButton(master=self.app, text="Select String File", command=self.select_file, fg_color="#0fa4af",
+        self.btn_select_string = CTkButton(master=self.app, text="Select String File", command=self.select_Stringfile, fg_color="#0fa4af",
                                    border_color='#0d737a',
                                    border_width=2, hover_color='#024950', text_color="#323231", height=40,
                                    font=(self.my_font2, 25))
@@ -68,7 +68,20 @@ class App:
         self.textbox.insert("0.0", sorted_df["number"].to_string(index=FALSE))
         print(sorted_df)
     
-    def select_file(self):
+    def sortString(self):
+        df = pd.read_csv(self.filename)
+
+        self.textbox.delete(0.0, END)
+
+        sorted_df = df.sort_values(by=["word"], ascending=True)
+        sorted_df.to_csv('homes_sorted.csv', index=False)
+        
+
+
+        self.textbox.insert("0.0", sorted_df["word"].to_string(index=FALSE))
+        print(sorted_df)
+    
+    def select_Numfile(self):
         filetypes = (
             ('text files', '*.csv'),
             ('All files', '*.*')
@@ -79,10 +92,26 @@ class App:
             initialdir='/',
             filetypes=filetypes)
 
-        
         self.sortNum()
 
         print(self.filename)
+    
+    def select_Stringfile(self):
+        filetypes = (
+            ('text files', '*.csv'),
+            ('All files', '*.*')
+        )
+
+        self.filename = fd.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes)
+
+        self.sortString()
+
+        print(self.filename)
+    
+
 
 
 App()
