a61249c9ae86527c5a91754b36a931a897e61e0a
AntiAntiexe <mrbricken@gmail.com>
2025-07-10 21:50:40 +1000

    add savedModelTotfLite.py to convert and save TensorFlow model as TFLite

---
+++ appliedComputing/groupProj/tools/savedModelTotfLite.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+# Convert the model
+converter = tf.lite.TFLiteConverter.from_saved_model("appliedComputing/groupProj/models") # path to the SavedModel directory
+tflite_model = converter.convert()
+
+# Save the model.
+with open('fruitModelMain.tflite', 'wb') as f:
+  f.write(tflite_model)
