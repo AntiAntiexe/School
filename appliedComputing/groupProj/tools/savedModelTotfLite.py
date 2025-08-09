962da886859c08343d6b9593f2895b8b36ea6a13
AntiAntiexe <mrbricken@gmail.com>
2025-08-09 13:05:21 +1000

    change directory

--- appliedComputing/groupProj/tools/savedModelTotfLite.py
+++ appliedComputing/groupProj/tools/savedModelTotfLite.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 # Convert the model
-converter = tf.lite.TFLiteConverter.from_saved_model("appliedComputing/groupProj/models") # path to the SavedModel directory
+converter = tf.lite.TFLiteConverter.from_saved_model("appliedComputing/groupProj/fruitModel") # path to the SavedModel directory
 tflite_model = converter.convert()
 
 # Save the model.
