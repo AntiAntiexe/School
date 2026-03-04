0d0d793350b35c3068aa353fea9a2dd6bfb044db
Anton <mrbricken@gmail.com>
2026-03-04 17:03:00 +1100

    fix selection sort

--- softwareDev34/Unit3/AOS1/selectionSort/selectionSort.py
+++ softwareDev34/Unit3/AOS1/selectionSort/selectionSort.py
@@ -18,10 +18,8 @@ class App:
             reader = csv.reader(file)
             for row in reader:
                 data.append(row)
-        
-        self.arr= [c[0] for c in data]
 
-        self.arr.sort(key=int)
+        self.arr= [int(c[0]) for c in data]
 
     def sortNumbers(self):
         n = len(self.arr)
@@ -29,15 +27,16 @@ class App:
         for i in range(n-2):
             minIndx = i
 
-            for j in range(i+1, n-1):
+            for j in range(i+1, n):
                 if self.arr[j] <= self.arr[minIndx]:
                     minIndx = j
-        self.arr[i], self.arr[minIndx] = self.arr[minIndx], self.arr[i]
+
+            self.arr[i], self.arr[minIndx] = self.arr[minIndx], self.arr[i]
 
 
         self.searchButton.place_forget()
 
-        self.label = ttk.Label(self.root, text=f"Sorted Numbers: {', '.join(self.arr)}")
+        self.label = ttk.Label(self.root, text=f"Sorted Numbers: {', '.join([str(item) for item in self.arr])}")
         self.label.place(x=300, y=100, anchor='center')
 
 
