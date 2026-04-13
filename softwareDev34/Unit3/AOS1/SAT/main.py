025f7f3696beff7e9bd47d4ccaff3990acc8651e
Anton <mrbricken@gmail.com>
2026-04-13 12:35:08 +1000

    changes

--- softwareDev34/Unit3/AOS1/SAT/main.py
+++
@@ -1,9 +0,0 @@
-# Source - https://stackoverflow.com/a/69519573
-# Posted by Vishnu, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-03-14, License - CC BY-SA 4.0
-
-import random 
-
-wordlist = ["a","b","c","d","e","f","g"] 
-print(random.sample(wordlist, 3))
-
